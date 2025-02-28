7ea43147bffc662238a2cb38ae41f7b21936eb43
Renandreolle <renandreolle@hotmail.com>
2025-02-28 18:33:26 -0300

    test: checando retornos de envs

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,12 @@ const routes: Array<RouteRecordRaw> = [
   },
 ]
 
+// Acessando variáveis de ambiente no lado do cliente com Vite
+console.log(import.meta.env.VITE_BASE_URL);  // A variável VITE_BASE_URL
+console.log(import.meta.env.VITE_DATABASE_URL);  // A variável VITE_DATABASE_URL
+console.log(import.meta.env.VITE_JWT_TOKEN);  // A variável VITE_JWT_TOKEN
+
+
 
 const router = createRouter({
   history: createMemoryHistory(import.meta.env.VITE_BASE_URL),
