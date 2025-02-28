323a69659ea76e1a3d60242382da8ec010917dba
Renandreolle <renandreolle@hotmail.com>
2025-02-28 17:12:58 -0300

    fix: ajustes de erros diversos na build

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
+// <reference types="vite/types/importMeta.d.ts" />
 
-interface ImportMetaEnv {
+declare interface ImportMetaEnv {
     readonly VITE_BASE_URL: string
     readonly VITE_DATABASE_URL: string
     readonly VITE_POSTGREST_URL: string
@@ -8,7 +9,7 @@ interface ImportMetaEnv {
     readonly VITE_JWT_TOKEN: string
   }
   
-  interface ImportMeta {
+declare interface ImportMeta {
     readonly env: ImportMetaEnv
   }
   
