5b7785ee46a37255f02ff6988d9d9cf156738b17
Renandreolle <renandreolle@hotmail.com>
2025-04-17 15:16:33 -0300

    fix: ajuste no serviço para ler os cursos

--- src/services/CourseSchoolService.ts
+++ src/services/CourseSchoolService.ts
@@ -13,17 +13,20 @@ export default class CourseSchoolService extends BaseService<TabelaType> {
     try {
       const { data, error } = await this.client
         .from(table)
-        .select('*')
+        .select('courseId')
         .eq('schoolId', schoolId)
 
       if (error) {
         console.error(error)
         return void 0
       }
-      else {
-        return data
+      const courses = await this.client
+        .from('course')
+        .select('*')
+        .in('id', data.map((course) => course.courseId))
+      
+      return courses.data
       }
-    }
     catch (error: unknown | any) {
       throw new Error(error)
     }
